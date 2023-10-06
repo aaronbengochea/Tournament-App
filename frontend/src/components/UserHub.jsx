255ea51ff4468b6ead4a3924f7e0e71448be597b
cicyfan <71156554+aaronbengochea@users.noreply.github.com>
2023-10-06 15:43:54 -0400

    new

--- frontend/src/components/UserHub.jsx
+++ frontend/src/components/UserHub.jsx
@@ -24,7 +24,7 @@ function UserHub() {
     return (
             <div className="FormCenter">
                 
-                    <p>Registration Form:</p>
+                    <p>Registration Form: Aaron</p>
             
             </div>
         );
