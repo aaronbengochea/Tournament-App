ce8a8f614636e4df00dea19336d83b32c3758a01
cicyfan <71156554+aaronbengochea@users.noreply.github.com>
2023-10-06 16:35:48 -0400

    Creating Tournament Page

---
+++ frontend/src/components/TournamentPage.jsx
@@ -0,0 +1,8 @@
+// Navigate from a tourney ID link in the UserHub & userID associated with the click
+// This page will house the tournament info for the given tourney
+    // such as: current entered player names, current bracket, elim type, game, etc
+// at the bottom of this page, we will have a match slip relative to the user in that given tourney so that they can report
+// this match slip only appears if there is an active match with that player at the moment
+
+
+// This page will operate to serve that function above for every tournament that you joined. 
