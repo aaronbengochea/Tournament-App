73e7eaf0e6e532b84b59e989bf7a5483eb5fa200
cicyfan <71156554+aaronbengochea@users.noreply.github.com>
2023-10-06 16:32:34 -0400

    Udate Userhub

--- frontend/src/components/UserHub.jsx
+++ frontend/src/components/UserHub.jsx
@@ -1,38 +1,83 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-
+import { NavLink } from 'react-router-dom';
 
 function UserHub() {
-    const localStoreID = localStorage.getItem('userID')
+  
+  const [userTournaments, setUserTournaments] = useState({ joined: [], created: [] });
 
 
+  useEffect(() => {
+    const localStoreID = localStorage.getItem('userID');
+    axios.get('http://localhost:4000/active_tournaments', {
+      params: {
+        userID: localStoreID
+      }
+    })
+    .then(function (res) {
+      setUserTournaments(res.data);
+    })
+    .catch(function (err) {
+      console.log(err);
+    });
+  }, []);
 
-    useEffect(() => {
-        axios.get('http://localhost:4000/activeTournamentCheck', {
-            params: {
-                userID: localStoreID
-            }
-        })
-        .then(function(res) {
-            console.log(res)
-        })
-        .catch(function(err){
-            console.log(err)
-        });
-    }, []);
-       
-    return (
-            <div className="FormCenter">
-                
-                    <p>Registration Form: Aaron</p>
-            
-            </div>
-        );
-    };
+  const handleTournamentIDClick = (tournamentID) => {
+      localStorage.setItem('clickedTournamentID', tournamentID);
+  };
 
+  return (
+    <div className="UserHub">
+      <h2>Your Tournaments</h2>
+      <div className="TournamentList">
+        <h3>Joined Tournaments:</h3>
+        <table>
+          <thead>
+            <tr>
+              <th className="table-cell">ID</th>
+              <th className="table-cell">Tournament Name</th>
+              <th className="table-cell">Player Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userTournaments.joined.map((tournament, index) => (
+              <tr key={index}>
+                <td className="table-cell">   
+                    <NavLink
+                        to="/tournamentPage"
+                        onClick={() => handleTournamentIDClick(tournament.id)}
+                    >
+                        {tournament.id}
+                    </NavLink>
+                </td>
+                <td className="table-cell">{tournament.tournament_name}</td>
+                <td className="table-cell">{tournament.player_total}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+        <h3>Created Tournaments:</h3>
+        <table>
+          <thead>
+            <tr>
+              <th className="table-cell">ID</th>
+              <th className="table-cell">Tournament Name</th>
+              <th className="table-cell">Player Total</th>
+            </tr>
+          </thead>
+          <tbody>
+            {userTournaments.created.map((tournament, index) => (
+              <tr key={index}>
+                <td className="table-cell">{tournament.id}</td>
+                <td className="table-cell">{tournament.tournament_name}</td>
+                <td className="table-cell">{tournament.player_total}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
 
-
-
-
-
-export {UserHub};
+export { UserHub };
