9235889bf3d049dee3b4073fae3332c75d576a8b
cicyfan <71156554+aaronbengochea@users.noreply.github.com>
2023-10-11 16:13:13 -0400

    Add tournament hub to index

--- frontend/src/components/index.js
+++ frontend/src/components/index.js
@@ -3,5 +3,6 @@ export {SignUp} from './SignUp'
 export {HomePage} from './HomePage'
 export {CreateTournament} from './CreateTournament'
 export {UserHub} from './UserHub'
+export {TournamentHub} from './TournamentHub'
 
 
