b795f63b468c98a53ecff7a17ec9480aed80f7cc
cicyfan <71156554+aaronbengochea@users.noreply.github.com>
2023-10-14 23:15:51 -0400

    Adding comments

---
+++ frontend/src/components/AdminTourneyBegin.jsx
@@ -0,0 +1,11 @@
+/* This will house the logic for the Admin force tourney creation
+
+ -The button will only appear if the tourney is yet to begin
+ -startParticipantCount will always be less then total participant count in these cases, we will need to fill a participants array with nulls 
+ -We will need to use helper functions such as balanceByes to balance our nulls
+ -We will create the json manager object
+ -We will store the tournamentID, manager object, id|participant mapping, etc for later use
+ -Tournament will render, button will fade
+ -Update DB field began = 1
+
+ */
