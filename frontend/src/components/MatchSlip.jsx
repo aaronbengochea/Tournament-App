9d34e1ea4e90cbbcaaad49a0e5523e799898affe
cicyfan <71156554+aaronbengochea@users.noreply.github.com>
2023-10-17 10:50:01 -0400

    Adding matchslip file

---
+++ frontend/src/components/MatchSlip.jsx
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import axios from 'axios';
+
+
+function MatchSlip(props) {
+    console.log(props.players)
+}
+
+    
+    return (
+            <div className="FormCenter">
+                <p>Match Slip</p>
+                
+            </div>
+        );
+
+
+
+export {MatchSlip};
