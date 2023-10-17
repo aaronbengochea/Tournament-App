14de0b2e59170cc74029dac169d255cc5c735267
cicyfan <71156554+aaronbengochea@users.noreply.github.com>
2023-10-16 21:18:33 -0400

    Update

--- frontend/src/components/TournamentHub.jsx
+++ frontend/src/components/TournamentHub.jsx
@@ -1,17 +1,13 @@
-// Navigate from a tourney ID link in the UserHub & userID associated with the click
-// This page will house the tournament info for the given tourney
-    // such as: current entered player names, current bracket, elim type, game, etc
-// at the bottom of this page, we will have a match slip relative to the user in that given tourney so that they can report
-// this match slip only appears if there is an active match with that player at the moment
-
-
-// This page will operate to serve that function above for every tournament that you joined. 
-
-
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { Viewer } from './Viewer';
+//import { useNavigate } from 'react-router-dom';
+import { BracketsManager } from 'brackets-manager';
+import { InMemoryDatabase } from 'brackets-memory-db';
+const { helpers } = require('brackets-manager');
 
+const storage = new InMemoryDatabase();
+const manager = new BracketsManager(storage);
 
 function TournamentHub() {
 
@@ -19,11 +15,85 @@ function TournamentHub() {
   const [tournamentInfo, setTournamentInfo] = useState([])
   const [createdByUserID, setCreatedByUserID] = useState(null)
   const [createdByGamertag, setCreatedByGamertag] = useState(null)
+  const [localStoreID, setLocalStoreID] = useState()
+  const [showAdminButton, setShowAdminButton] = useState(false)
+  //const navigate = useNavigate();
   
+  const handleAdminForceStart = async () => {
+
+    const playerCount = players.length
+    const roundedPlayerCount = helpers.getNearestPowerOfTwo(playerCount)
+    console.log(players)
+
+    const filledPlayerArray = []
+
+    for (let i = 0; i < playerCount; i++){
+      filledPlayerArray.push(players[i].gamertag)
+    }
+
+    while (filledPlayerArray.length < roundedPlayerCount - 1) {
+      filledPlayerArray.push(null)
+    }
+
+    const updatedPlayerArray = helpers.balanceByes(filledPlayerArray, roundedPlayerCount)
+
+    try {
+      await manager.create.stage({
+          name: tournamentInfo.tournament_name,
+          tournamentId: 0, //could retrieve from db at initialization
+          type: "single_elimination",
+          seeding: updatedPlayerArray,
+          settings: {
+              seedOrdering: ["natural"],
+              balanceByes: false,
+              size: roundedPlayerCount,
+              matchesChildCount: 0,
+              consolationFinal: false
+          }
+      })
+    } catch (error) {
+      console.error("error creating tourney")
+    }
+
+    const tournamentState = await manager.get.stageData(0)
+    console.log(tournamentState)
+
+
+    const participantTable = tournamentState.participant
+
+    const idNameMap = participantTable.reduce((map,participant) => {
+      map[participant.id] = participant.name;
+      return map;
+    },{})
+
+    console.log(idNameMap)
+
+    
+    const formData2 = {
+      t_id: tournamentInfo.id,
+      t_state: tournamentState,
+      p_idMap: idNameMap,
+    }
+
+    axios({
+      method: 'POST',
+      url: 'http://localhost:4000/create_tournament_object',
+      data: formData2,
+      config: { headers: { 'Content-Type': 'multipart/form-data' } },
+    })
+    .then(function (response) {
+        console.log("Backend t_state Store: ", response)
+        window.location.reload();
+    })
+    .catch()
+    
+}
 
   useEffect(() => {
     const localStoreID = localStorage.getItem('userID');
     const localClickedTournamentID = localStorage.getItem('clickedTournamentID')
+    setLocalStoreID(localStoreID)
+
 
     axios.get('http://localhost:4000/tournament_loader', {
       params: {
@@ -48,9 +118,13 @@ function TournamentHub() {
       if (createdByPlayer){
         setCreatedByGamertag(createdByPlayer.gamertag)
         console.log(createdByPlayer.gamertag)
+
+        if (createdByUserID === localStoreID && tournamentInfo.began === 0) {
+          setShowAdminButton(true)
+        }
       }
     }
-  }, [createdByUserID, players]);
+  }, [createdByUserID, players, localStoreID, tournamentInfo]);
 
   //build in check so that it only attempts to use Viewer when the began is set to yes
 
@@ -68,6 +142,11 @@ function TournamentHub() {
       <p>Completed? {tournamentInfo.completed === 0 ? 'No' : 'Yes'} </p>
       <p>Invite Only? {tournamentInfo.invite_only}</p>
       <p>Current Round: </p>
+      {showAdminButton && (
+        <button onClick={handleAdminForceStart}>
+          Admin Button
+        </button>
+      )}
       <p></p>
       <h3>Participants:</h3>
       <ul>
@@ -77,9 +156,10 @@ function TournamentHub() {
           </li>
         ))}
       </ul>
+      
       <div>
         
-        <Viewer></Viewer>
+        {tournamentInfo.began === 1 && <Viewer/>}
       </div>
     </div>
   );
