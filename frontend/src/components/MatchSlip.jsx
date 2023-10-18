6078a486c7a2a213d8248c2df804c8baf9c3f5ec
cicyfan <71156554+aaronbengochea@users.noreply.github.com>
2023-10-17 22:58:43 -0400

    Update

--- frontend/src/components/MatchSlip.jsx
+++ frontend/src/components/MatchSlip.jsx
@@ -1,19 +1,22 @@
-import React, {useState} from 'react';
-import axios from 'axios';
+import React /* , {useState} */ from 'react';
+//import axios from 'axios';
 
 
 function MatchSlip(props) {
-    console.log(props.players)
-}
+    //if began = 1
+    //take tourney object and feed it to bracket manager
+    //use manager.get.currentRound --> will need tourney id (0) to fetch stage, will need stage.id to fetch matches
+    console.log("match slip")
+    console.log(props.playerList)
+    console.log(props.tInfo)
 
-    
     return (
-            <div className="FormCenter">
-                <p>Match Slip</p>
-                
-            </div>
-        );
-
-
+        <div className="FormCenter">
+            <p>Match Slip</p>
+            
+            
+        </div>
+    );
+}
 
 export {MatchSlip};
