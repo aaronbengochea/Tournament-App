7de9518521953d2658f381ce27084eec945af275
cicyfan <71156554+aaronbengochea@users.noreply.github.com>
2023-10-24 12:54:29 -0400

    Update

---
+++ frontend/src/components/AdminTourneyProgression.jsx
@@ -0,0 +1,9 @@
+//This component will display all match couples and their respective round reported scores, the admin will have the ability to 
+//manually overide any score submitted, we will use some form of CSS to assist the admin in deciding if scores are submitted properly
+
+
+//I plan on first implementing a manual progression system for admins since this general system will remain useful beyond the implementation of auto-completed rounds
+
+//Reasons for manual progression: 
+//1) if a player does not respond or report, this will allow the admin to intervene and progress the tournament
+//2) if a player mistakenly or willingly reports an incorrect score which would directly affect the state of the tournament, allows the admin a layer of intervention 
