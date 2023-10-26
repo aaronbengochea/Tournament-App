1384950da31ddd89e9d2d46a3d0c1d1625fd334d
cicyfan <71156554+aaronbengochea@users.noreply.github.com>
2023-10-26 16:51:52 -0400

    Update

--- frontend/src/components/AdminTourneyProgression.jsx
+++ frontend/src/components/AdminTourneyProgression.jsx
@@ -1,9 +1,17 @@
-//This component will display all match couples and their respective round reported scores, the admin will have the ability to 
-//manually overide any score submitted, we will use some form of CSS to assist the admin in deciding if scores are submitted properly
+/*
+
+This component will display all match couples and their respective round reported scores, the admin will have the ability to 
+manually overide any score submitted, we will use some form of CSS to assist the admin in deciding if scores are submitted properly
 
 
-//I plan on first implementing a manual progression system for admins since this general system will remain useful beyond the implementation of auto-completed rounds
+I plan on first implementing a manual progression system for admins since this general system will remain useful beyond the implementation of auto-completed rounds
 
-//Reasons for manual progression: 
-//1) if a player does not respond or report, this will allow the admin to intervene and progress the tournament
-//2) if a player mistakenly or willingly reports an incorrect score which would directly affect the state of the tournament, allows the admin a layer of intervention 
+Reasons for manual progression: 
+1) if a player does not respond or report, this will allow the admin to intervene and progress the tournament
+2) if a player mistakenly or willingly reports an incorrect score which would directly affect the state of the tournament, allows the admin a layer of intervention 
+
+---NEW---
+I want to incorporate auto-updating to the scoring system, so that this component only has to handle non-balanced scores
+I will need to change the matchslip component to handle score checking on each submission, if the opponent score is non-null, then compare the two reported scores and update the tournament object with a winner using match.update function
+
+*/
